b3e3827ed6d283a95d9caa98c13b61d988568213
Marius <marius@example.com>
2022-04-01 12:05:32 +0300

    makeMeEl

--- js/modules/helper.js
+++ js/modules/helper.js
@@ -15,3 +15,8 @@ export function checkErrors(errorObj) {
 export function checkErrors2(errorObj) {
   return Object.values(errorObj).some((reiks) => reiks.length > 0);
 }
+
+// sukurti funkcija kuri priima argumentu htmlTaga, texta, clasesVarda ir tevine html elementa.
+// sukuria elementa ir ideda i tevini el
+makeMeEl('div', '', 'card', appEl);
+makeMeEl('p', 'kazoks text', 'card__text', divEl);
