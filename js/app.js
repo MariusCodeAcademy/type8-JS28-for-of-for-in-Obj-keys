7c247f3d8089562bebbb1e01ec2113e0f57d804d
Marius <marius@example.com>
2022-04-01 09:34:56 +0300

    sudeti i masyva visas user reiksmes uzd

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+import { user } from './modules/data.js';
 console.log('app');
 
 // iskonsolinti user objekta app.js
+console.log('user ===', user);
+
+// for (key in object) {
+
+// atspausdinti visus user keys
+
+for (let key in user) {
+  console.log('key ===', key);
+}
+const keysArr = [];
+for (let key in user) {
+  keysArr.push(key);
+}
+console.log('keysArr ===', keysArr);
+
+// atspaustindi visas user objekto reiksmes
+for (let key in user) {
+  console.log(user[key]);
+}
+
+// sudeti i masyva visas user reiksmes
--- js/modules/data.js
+++ js/modules/data.js
@@ -1,5 +1,20 @@
-const user = {
+export const user = {
   name: 'Mike',
   age: 25,
   hasCar: true,
+};
+const key = 'name';
+console.log('user[key]  ===', user[key]);
+
+const errors1 = {
+  name: '',
+  password: '',
+  town: 'cant be blank',
+};
+
+const newUserObj = {
+  username: 'Mike545',
+  password: '123456',
+  gender: 'male',
+  country: '',
 };
